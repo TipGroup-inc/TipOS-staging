84042d3bd55d2e50d5196903dd11377b8714b722
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-25 00:14:41 -0300

    kernel: tss.h ~~ - se quebrar nao foi eu

---
+++ OvsbMk/kernel/tss.h
@@ -0,0 +1,23 @@
+/* moe moe kyun <3 */
+/* ♥ kernel stuff ~ aqui é onde o bicho pega de verdade! kyun~
+ * arquivo: tss.h ~ funcoes anotadas: 0
+ */
+/* ♥ tss.h ~ feito com carinho (e gambiarras) pela equipe TipOS! ♥
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#ifndef TSS_H
+#define TSS_H
+#include <stdint.h>
+
+void tss_init(void);
+void tss_set_rsp0(uint64_t rsp0);
+
+#endif
+
+/* ♥ tss.h ~ arquivo fofinho do OvsbMkM! kyun~ <3 */
+
+
+
+
+/* ♥ tss.h ~ se bugar me chama, se n bugar tb me chama ~ >u< */
