dc3b35a3a30796d85ebf999a92ac432e451a2a0d
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-25 07:35:30 -0300

    drivers: ata.h ~~ só mais um chato

---
+++ OvsbMk/drivers/ata.h
@@ -0,0 +1,23 @@
+/* moe moe kyun <3 */
+/* ♥ driver ~ conversando com o hardware, seu chato!
+ * arquivo: ata.h ~ funcoes anotadas: 0
+ */
+/* ♥ ata.h ~ feito com carinho (e gambiarras) pela equipe TipOS! ♥
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+/* ♥ ATA_H ~ "Header do driver ATA~"
+ * Dica: ata_read_sector(lba, buf) ~ lê um setor de 512 bytes~
+ * Se o LBA for maior que o disco, vai dar ruim~ */
+#ifndef ATA_H
+#define ATA_H
+#include <stdint.h>
+void ata_init(void);
+int ata_read_sector(uint32_t lba, uint8_t *buffer);
+int ata_write_sector(uint32_t lba, const uint8_t *buffer);
+#endif
+
+
+
+
+/* ♥ ata.h ~ se bugar me chama, se n bugar tb me chama ~ >u< */
