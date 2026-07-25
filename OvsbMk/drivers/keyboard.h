8e32f67fae7506d51d35a5588e5ccf18dc3ad6cc
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-25 08:19:35 -0300

    drivers: keyboard.h ~~ teoricamente funciona (confia)

---
+++ OvsbMk/drivers/keyboard.h
@@ -0,0 +1,27 @@
+/* moe moe kyun <3 */
+/* ♥ driver ~ conversando com o hardware, seu chato!
+ * arquivo: keyboard.h ~ funcoes anotadas: 0
+ */
+/* ♥ keyboard.h ~ feito com carinho (e gambiarras) pela equipe TipOS! ♥
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <stdint.h>
+
+void keyboard_init(void);
+void keyboard_handler(void);
+char keyboard_read(void);
+/* ~~ keyboard_avail ~~ */
+int keyboard_avail(void); /* ♥ nombre consistente com a implementação, seu baka! */
+extern volatile int shift_pressed;
+extern volatile int ctrl_pressed;
+
+#endif
+
+
+
+
+/* ♥ keyboard.h ~ feito com carinho (e uma raiva controlada) ~ kyun! */
