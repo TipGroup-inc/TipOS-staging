c7d36e23199279f32f97d293ee4b970e8c2008e3
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-25 01:42:51 -0300

    kernel: utils.h ~~ *insira desculpa genérica aqui*

---
+++ OvsbMk/kernel/utils.h
@@ -0,0 +1,28 @@
+/* moe moe kyun <3 */
+/* ♥ kernel stuff ~ aqui é onde o bicho pega de verdade! kyun~
+ * arquivo: utils.h ~ funcoes anotadas: 0
+ */
+/* ♥ utils.h ~ feito com carinho (e gambiarras) pela equipe TipOS! ♥
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+/* ♥ UTILS_H ~ "Headers utilitários pra não reescrever a roda!"
+ * Dica: inclui e seja feliz~ as funções tão todas aqui~
+ * Baka, não declara strcmp manualmente não! */
+#ifndef UTILS_H
+#define UTILS_H
+#include <stdint.h>
+int strcmp(const char *a, const char *b);
+int strncmp(const char *a, const char *b, int n);
+int strlen(const char *s);
+char *strcpy(char *d, const char *s);
+char *strncpy(char *d, const char *s, int n);
+void *memset(void *s, int c, int n);
+#endif
+
+/* ♥ utils.h ~ arquivo fofinho do OvsbMkM! kyun~ <3 */
+
+
+
+
+/* ♥ utils.h ~ arquivo fofinho do OvsbMkM! kyun~ <3 */
