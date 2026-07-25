387364aeee3651f1ccc7734ca96640b3fdb22656
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-24 22:46:31 -0300

    kernel: shell.h ~~ funciona por algum milagre

---
+++ OvsbMk/kernel/shell.h
@@ -0,0 +1,20 @@
+/* moe moe kyun <3 */
+/* ♥ kernel stuff ~ aqui é onde o bicho pega de verdade! kyun~
+ * arquivo: shell.h ~ funcoes anotadas: 0
+ */
+/* ♥ shell.h ~ feito com carinho (e gambiarras) pela equipe TipOS! ♥
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#ifndef SHELL_H
+#define SHELL_H
+
+void shell_init(void);
+void shell_input(char c);
+
+#endif
+
+
+
+
+/* ♥ shell.h ~ arquivo fofinho do OvsbMkM! kyun~ <3 */
