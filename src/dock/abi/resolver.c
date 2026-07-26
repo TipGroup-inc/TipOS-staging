9162f71350b0122b286d1402c619ed76a3259839
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-26 19:35:30 -0300

    dock: resolver.c ~~ foi sem querer juro >_<

---
+++ src/dock/abi/resolver.c
@@ -0,0 +1,15 @@
+/* moe moe kyun <3 */
+/* ♥ dock ~ gerenciando app/processo, baka fique quieto!
+ * arquivo: resolver.c ~ funcoes anotadas: 0
+ */
+/* ~*~ resolver.c ~*~
+ * Hihi, olha esse arquivo aqui~ Que lindo, né? >_<
+ * Escrito com muito amor (e gambiarras) pela equipe TipOS!
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+
+
+
+
+/* ♥ resolver.c ~ feito com carinho (e uma raiva controlada) ~ kyun! */
