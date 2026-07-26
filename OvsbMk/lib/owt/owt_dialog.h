3aa324a84a5e1aaccde1c50c10ec49e68111566f
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-26 04:09:47 -0300

    gui: owt_dialog.h ~~ - se quebrar nao foi eu

---
+++ OvsbMk/lib/owt/owt_dialog.h
@@ -0,0 +1,21 @@
+/* moe moe kyun <3 */
+/* ♥ OWT widget ~ toolkit bonitinho pra desenhar coisa na tela!
+ * arquivo: owt_dialog.h ~ funcoes anotadas: 0
+ */
+/* ~*~ owt_dialog.h ~ "Dialogos ~ pergunta se o usuario tem certeza!" ~*~
+ * Funcoes pra criar dialogos modais: message (so avisa) e confirm
+ * (pergunta sim/nao). Centraliza automaticamente na tela usando
+ * wm_get_scr_w/h() - sem hardcode de 1024x768! kyun~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#ifndef OWT_DIALOG_H
+#define OWT_DIALOG_H
+
+void owt_dialog_message(const char *title, const char *message);
+int owt_dialog_confirm(const char *title, const char *message);
+
+#endif
+
+
+
+/* ♥ owt_dialog.h ~ se bugar me chama, se n bugar tb me chama ~ >u< */
