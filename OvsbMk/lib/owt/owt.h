1ce2ec29ff9a5428d2b42eccfa3f24ca3c9e2302
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-26 01:13:27 -0300

    gui: owt.h ~~ só mais um chato

---
+++ OvsbMk/lib/owt/owt.h
@@ -0,0 +1,29 @@
+/* moe moe kyun <3 */
+/* ♥ OWT widget ~ toolkit bonitinho pra desenhar coisa na tela!
+ * arquivo: owt.h ~ funcoes anotadas: 0
+ */
+/* ~*~ owt.h ~ "OWT - Oh Window Toolkit! (ou OvsbMk Widget Toolkit)" ~*~
+ * Header unificado que inclui todos os headers do OWT. É so incluir
+ * esse e pronto~ tem base, draw, theme, window, label, button, textbox,
+ * statusbar, listview, combobox, menu, dialog. Tudo que um dev precisa
+ * pra fazer uma GUI ~moe~ no TipOS! Se faltar algum widget, faz voce
+ * mesmo~ >_< nyan! <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#ifndef OWT_H
+#define OWT_H
+#include "owt_base.h"
+#include "owt_draw.h"
+#include "owt_theme.h"
+#include "owt_window.h"
+#include "owt_label.h"
+#include "owt_button.h"
+#include "owt_textbox.h"
+#include "owt_statusbar.h"
+#include "owt_listview.h"
+#include "owt_combobox.h"
+#include "owt_menu.h"
+#include "owt_dialog.h"
+#endif
+
+/* ♥ owt.h ~ se bugar me chama, se n bugar tb me chama ~ >u< */
