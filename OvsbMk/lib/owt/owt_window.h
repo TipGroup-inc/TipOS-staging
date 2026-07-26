9825856ddb71fda60b90b7f8e9ef142e9545ea0b
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-26 10:02:26 -0300

    gui: owt_window.h ~~ lá vamos nós com owt_window.h de novo

---
+++ OvsbMk/lib/owt/owt_window.h
@@ -0,0 +1,30 @@
+/* moe moe kyun <3 */
+/* ♥ OWT widget ~ toolkit bonitinho pra desenhar coisa na tela!
+ * arquivo: owt_window.h ~ funcoes anotadas: 0
+ */
+/* ~*~ owt_window.h ~ "Janela OWT com titulo e conteudo!" ~*~
+ * Uma janela que tem titulo, pode ter botao de fechar e um widget
+ * de conteudo interno. Usa owt_widget_t como base (heranca via
+ * composicao, porque C nao tem class~ #F). Se quebrar, foi mal! <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#ifndef OWT_WINDOW_H
+#define OWT_WINDOW_H
+#include "owt_base.h"
+
+typedef struct {
+    owt_widget_t base;
+    char title[128];
+    int can_close;
+    owt_widget_t *content;
+} owt_window_t;
+
+owt_window_t *owt_window_create(const char *title, int x, int y, int w, int h);
+void owt_window_set_content(owt_window_t *win, owt_widget_t *content);
+void owt_window_draw(owt_window_t *win);
+
+#endif
+
+
+
+/* ♥ owt_window.h ~ arquivo fofinho do OvsbMkM! kyun~ <3 */
