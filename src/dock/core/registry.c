3beade83defa61b1a50baa7435aef512c9a64e14
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-26 22:31:49 -0300

    dock: registry.c ~~ *insira desculpa genérica aqui*

---
+++ src/dock/core/registry.c
@@ -0,0 +1,16 @@
+/* moe moe kyun <3 */
+/* ♥ dock ~ gerenciando app/processo, baka fique quieto!
+ * arquivo: registry.c ~ funcoes anotadas: 0
+ */
+/* ~*~ registry.c ~*~
+ * Hihi, olha esse arquivo aqui~ Que lindo, né? >_<
+ * Escrito com muito amor (e gambiarras) pela equipe TipOS!
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#include "dock.h"
+
+
+
+
+/* ♥ registry.c ~ feito com carinho (e uma raiva controlada) ~ kyun! */
