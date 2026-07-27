dc9a88af819c49790438a69efac7e8dc67a6fb08
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-27 05:52:38 -0300

    userland: disp.h ~~ foi sem querer juro >_<

---
+++ src/userland/disp/disp.h
@@ -0,0 +1,21 @@
+/* moe moe kyun <3 */
+/* ♥ userland ~ programinha de ring 3, longe do kernel!
+ * arquivo: disp.h ~ funcoes anotadas: 0
+ */
+/* ~*~ disp.h ~*~
+ * Hihi, olha esse arquivo aqui~ Que lindo, né? >_<
+ * Escrito com muito amor (e gambiarras) pela equipe TipOS!
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#ifndef DISP_H
+#define DISP_H
+int disp_create_window(int x, int y, int w, int h, const char *title);
+void disp_update_mouse(int dx, int dy);
+void disp_render(void);
+void disp_init(void);
+#endif
+
+
+
+/* ♥ disp.h ~ arquivo fofinho do OvsbMkM! kyun~ <3 */
