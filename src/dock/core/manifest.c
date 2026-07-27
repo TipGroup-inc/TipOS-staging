7f2cc98c64a35c67b7a79bbf8d9db0b1c2e01e48
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-26 21:47:44 -0300

    dock: manifest.c ~~ hmm sei la, compilei e rodou

---
+++ src/dock/core/manifest.c
@@ -0,0 +1,15 @@
+/* moe moe kyun <3 */
+/* ♥ dock ~ gerenciando app/processo, baka fique quieto!
+ * arquivo: manifest.c ~ funcoes anotadas: 0
+ */
+/* ~*~ manifest.c ~*~
+ * Hihi, olha esse arquivo aqui~ Que lindo, né? >_<
+ * Escrito com muito amor (e gambiarras) pela equipe TipOS!
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+
+
+
+
+/* ♥ manifest.c ~ arquivo fofinho do OvsbMkM! kyun~ <3 */
