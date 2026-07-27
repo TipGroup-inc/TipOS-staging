6165cf0d614f9f8b1e155fd1eae7bcf1ceeff000
Haruna Himekawa <whimekasyharuna@yahoo.com>
2026-07-27 05:08:33 -0300

    dock: dock.h ~~ porque eu tenho que fazer isso

---
+++ src/dock/include/dock.h
@@ -0,0 +1,30 @@
+/* moe moe kyun <3 */
+/* ♥ dock ~ gerenciando app/processo, baka fique quieto!
+ * arquivo: dock.h ~ funcoes anotadas: 0
+ */
+/* ~*~ dock.h ~*~
+ * Hihi, olha esse arquivo aqui~ Que lindo, né? >_<
+ * Escrito com muito amor (e gambiarras) pela equipe TipOS!
+ * Se quebrar, a culpa é sua~ <3
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+
+#ifndef DOCK_H
+#define DOCK_H
+
+#define DOCK_VERSION "0.1.0"
+
+typedef enum {
+    DOCK_OK      = 0,
+    DOCK_ERR_MEM = -1,
+    DOCK_ERR_ABI = -2,
+    DOCK_ERR_MANIFEST = -3,
+    DOCK_ERR_LOAD = -4,
+    DOCK_ERR_INIT = -5,
+} dock_err_t;
+
+#endif
+
+
+
+
+/* ♥ dock.h ~ feito com carinho (e uma raiva controlada) ~ kyun! */
